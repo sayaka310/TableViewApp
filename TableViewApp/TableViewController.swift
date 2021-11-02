61a3e28dd837886502011874ac34a0f81899daff
sayaka310 <syk3887@yahoo.co.jp>
2021-11-02 13:11:14 +0900

    削除機能追加完了

--- TableViewApp/TableViewController.swift
+++ TableViewApp/TableViewController.swift
@@ -66,25 +66,24 @@ class TableViewController: UITableViewController {
     }
     
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            let userDefaults = UserDefaults.standard
+            taskArray.remove(at: indexPath.row)
+            userDefaults.set(taskArray, forKey: "add")
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
